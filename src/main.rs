408ac840f37ff3ed88ceba0be767bd9dbb777ca6
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-09-27 12:44:27 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ fn main(){
 	let x=Value::from(x);															// create a Value from the vector. Value stores a tensor of either bool, float, int, or multi, or a error, and may have rank from 1-8
 				// connect a node labeled "x" to a node labeled "y" using a linear layer. These labels don't have to correspond to variable names, but they are kind of like variable names for the inside of the graph
 
-	
-														// unwrap a rank 1 float tensor from y to print it. (i apparently forgot to implement display for Value, when I do we'll be able to print the value directly)
-	graph.connect("x",1_usize).with(Layer::linear(true,28*28,64,1.0));	
-	graph.connect(1_usize,2_usize).with(Layer::relu());	
-	graph.connect(2_usize,3_usize).with(Layer::linear(true,64,32,1.0));	
-	graph.connect(3_usize,4_usize).with(Layer::relu());	
-	graph.connect(4_usize,"y").with(Layer::linear(true,32,10,1.0));
 
-	let y=Unvec(dbg!(&graph)).forward(x);													// apply the graph network by reference to x. AI trait contains the forward method, and is implemented for references too. Since graph can either take a hashmap or vector of inputs, letting it directly have single inputs caused type issues, so Unvec is a convenience wrapper for putting the input in a vec and taking the output out of the vec
+														// unwrap a rank 1 float tensor from y to print it. (i apparently forgot to implement display for Value, when I do we'll be able to print the value directly)
+	graph.connect("x",1_usize).with_clear(true).with(Layer::linear(true,28*28,64,1.0));
+	graph.connect(1_usize,2_usize).with_clear(true).with(Layer::relu());
+	graph.connect(2_usize,3_usize).with_clear(true).with(Layer::linear(true,64,32,1.0));
+	graph.connect(3_usize,4_usize).with_clear(true).with(Layer::relu());
+	graph.connect(4_usize,"y").with_clear(true).with(Layer::linear(true,32,10,1.0));
+
+	let y=Unvec(&graph).forward(x);													// apply the graph network by reference to x. AI trait contains the forward method, and is implemented for references too. Since graph can either take a hashmap or vector of inputs, letting it directly have single inputs caused type issues, so Unvec is a convenience wrapper for putting the input in a vec and taking the output out of the vec
 	println!("{}",y.unwrap_f1());
 }
 use block_graph::{																	// imports from block-graph
